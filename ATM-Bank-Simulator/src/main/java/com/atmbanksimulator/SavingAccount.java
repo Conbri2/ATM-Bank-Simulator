d577690fc94e79a2696731d69fdb6e7a9945d19a
conno <C.Bridgman1@uni.brighton.ac.uk>
2026-05-05 22:20:55 +0100

    yes

--- ATM-Bank-Simulator/src/main/java/com/atmbanksimulator/SavingAccount.java
+++ ATM-Bank-Simulator/src/main/java/com/atmbanksimulator/SavingAccount.java
@@ -5,13 +5,16 @@
 
 package com.atmbanksimulator;
 
-public class SavingAccount extends BankAccount {
-    public SavingAccount(String var1, String var2, int var3) {
-        super(var1, var2, var3);
-    }
+public class SavingAccount extends BankAccount { // Creating Savings account with additional withdrawl rules.
+    public SavingAccount(String var1, String var2, int var3) { /* Calls BankAccount constructor and passes account number,
+     pin number, and balance details. */
 
+        super(var1, var2, var3); //calls BankAccount constructor
+    }
     @Override
-    public boolean withdraw(int var1) {
-        return var1 > 300 ? false : super.withdraw(var1);
+    public boolean withdraw(int var1) { // Overrides withdraw method from BankAccount to add additional rules.
+
+        return var1 > 300 ? false : super.withdraw(var1); /* If amount is greater than 300, it denies withdrawl, otherwise
+        it uses the original method from Bank. */
     }
 }
